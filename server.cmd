ab6ec222f23fae436ade268dba6dd15adbd446c5
Pavan <itspanzi@gmail.com>
2011-01-18 23:07:20 +0530

    Added a start and stop batch file. The reason it is different from server.sh because there is no notion of disown, backgrounding, redirect outputs, head command etc. Basically, we leave a shell running all the time and that works as the status check. Lets see if this is an issue and people crib about this

---
+++ server.cmd
@@ -0,0 +1,11 @@
+@echo off
+
+REM uncomment statements to override variable values, comments show the default
+
+REM The port the TLB server would listen to
+REM set TLB_PORT=7019 
+
+REM Store is the directory under which the test information(running-time, results etc) are stored
+REM set tlb_store=tlb_store #in current working directory
+
+java -jar tlb-all*.jar
