42a787c1df974a0053c9ae964d08b61cff64f253
Pavan <itspanzi@gmail.com>
2011-01-19 23:50:55 +0530

    Made the messaging on the batch file consistant with server.sh. Also, make sure the current directory is a part of the title so that we can have more than one server running at a time. .server.pid file acts a lock file so that more than one server cannot be started from the same folder. Cleanup does the same thing as the server.sh.

--- server.bat
+++ server.bat
@@ -4,7 +4,6 @@ set pid_file=.server.pid
 
 if "%1%"=="start" (
   call :start_server
-  echo %PID% > %pid_file%
 ) else if "%1%"=="stop" (
   call :stop_server
 ) else if "%1%"=="status" (
@@ -25,38 +24,58 @@ goto :EOF
   echo Starting TLB server
   if exist %pid_file% (
     echo PID file: %pid_file% already exists. It seems a TLB server is already running off this directory.
-    set %pid_in_file%=type %pid_file%
-    echo The process ID of this process, according to the PID file is: %pid_in_file%
+    for /f "usebackq delims==" %%a in (`type %pid_file%`) do set pid_from_file=%%a
+    echo The process ID of this process, according to the PID file is: %pid_from_file%
     echo Please stop this process using 'server.bat stop' or cleanup using 'server.bat cleanup' to have this PID file removed
     exit /B 1
   )
   START "Test Load Balancer Server in %cd%" /MIN server.cmd
+  echo %PID% > %pid_file%
   call :populate_pid
   echo Server started. The PID is: %PID%
 goto :EOF
 
 :status
-  if %PID% neq "NO" set not_running=true
-  if %PID% neq "No" set not_running=true 
-  if %not_running%=="true" (
-    echo TLB Server is running with process ID: %PID%
+  call :is_server_running
+  if "%server_running%"=="true" (
+    echo TLB server is running with process ID: %PID%
+  ) else (
+    echo TLB server is not running
   )
 goto :EOF
 
 :stop_server
-  echo Stopping TLB server
-  call :populate_pid
-  if %PID% neq "NO" (
-    TASKKILL /F /T /PID %PID%
+  call :is_server_running
+  if "%server_running%"=="true" (
+    call :kill_server
+  ) else (
+    echo Does not look like TLB server is running
   )
+goto :EOF
+
+:kill_server
+  call :populate_pid
+  TASKKILL /F /T /PID %PID%
   call :delete_pid_file
   echo Stopped TLB server
 goto :EOF
 
 :populate_pid
+  set PID=
   FOR /F "tokens=2" %%I in ('TASKLIST /NH /FI "WINDOWTITLE eq Test Load Balancer Server in %cd% - server.cmd"' ) DO set PID=%%I
 goto :EOF
 
 :delete_pid_file
   del /Q /F %pid_file%
+goto :EOF
+
+:is_server_running
+  call :populate_pid
+  if %PID% neq "NO" set "running=true"
+  if %PID% neq "No" set "running=true"
+  if "%running%"=="true" (
+    set "server_running=true"
+  ) else  (
+    set "server_running=false"
+  )
 goto :EOF
